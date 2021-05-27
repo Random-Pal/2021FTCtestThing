5b55b60671c9537e60df654fba7bce6c932872da
Random-Pal <randompal320@gmail.com>
2021-05-26 23:35:36 -0400

    I have to tell you something me, what you attempted FAILED. but i know you're not surprised b/c you are me. REGARDLESS MAYBE THIS TIME?!?!?!?!??!? WHO KNOWS MAN

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ANOTHERtestFolderThing/anotherTest.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.ANOTHERtestFolderThing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+
+public class anotherTest extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    PIDCoefficients something;
+
+
+    @Override
+    public void runOpMode()  {
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+            //making change so that i can commit
+
+        }
+    }
+}
